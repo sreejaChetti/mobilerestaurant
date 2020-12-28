8de947abf24948767ea39ade2154fc9f854e090f
sreejaChetti <74004373+sreejaChetti@users.noreply.github.com>
2020-12-28 14:57:58 +0530

    Add files via upload

---
+++ precache-manifest.e147d617dd1a8edc6621c19274ee1ab9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "e706481c8be34ddfe2c2",
+    "url": "/static/css/main.9f5d9dc2.chunk.css"
+  },
+  {
+    "revision": "e706481c8be34ddfe2c2",
+    "url": "/static/js/main.d9363707.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdab08c08962f5923335",
+    "url": "/static/css/2.69abddf7.chunk.css"
+  },
+  {
+    "revision": "fdab08c08962f5923335",
+    "url": "/static/js/2.b3473463.chunk.js"
+  },
+  {
+    "revision": "5178951d05fa405faebf5933fd21d979",
+    "url": "/static/media/bg7.5178951d.jpg"
+  },
+  {
+    "revision": "91ba92eec34536cc8679ef486d1303cc",
+    "url": "/static/media/home7.91ba92ee.jpeg"
+  },
+  {
+    "revision": "b50014a37f629c5343ef07e98b744a52",
+    "url": "/static/media/bang.b50014a3.jpg"
+  },
+  {
+    "revision": "382e4b72d4abf30201416d0b12007560",
+    "url": "/static/media/mumbai.382e4b72.jpg"
+  },
+  {
+    "revision": "0a0d0ec6fab808bcea6e4d9d0789f2ea",
+    "url": "/static/media/grid4.0a0d0ec6.jpg"
+  },
+  {
+    "revision": "a26afd9284c72e916323968e213d65f6",
+    "url": "/static/media/onlineFood1.a26afd92.jpg"
+  },
+  {
+    "revision": "8d27fd45aa2770e4c90bbe6278b15a6e",
+    "url": "/static/media/onlineFood2.8d27fd45.jpg"
+  },
+  {
+    "revision": "2e50a45370a2954618affd8eb53e12a4",
+    "url": "/static/media/onlineFood3.2e50a453.jpg"
+  },
+  {
+    "revision": "d95408cffa5ddc0e0f253cdacbebb1eb",
+    "url": "/static/media/about.d95408cf.jpg"
+  },
+  {
+    "revision": "a978178e7bb41c6b8401b3c3fdf497af",
+    "url": "/static/media/foodMobile.a978178e.jpg"
+  },
+  {
+    "revision": "ad72785ed71cd5d7c07565d98c4f4ad9",
+    "url": "/static/media/chef.ad72785e.jpg"
+  },
+  {
+    "revision": "c02844c2979b62b8cc56d3e12d01ffcf",
+    "url": "/static/media/contact.c02844c2.jpg"
+  },
+  {
+    "revision": "afe340a2bb109900c84f4e5e69688538",
+    "url": "/static/media/restuarent-app.afe340a2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "87612431ea874ca934bf71809df87f6f",
+    "url": "/index.html"
+  }
+];
